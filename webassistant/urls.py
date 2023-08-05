2eb8e9fd637d51c8863983afd268fd075d3ffb63
David Huy Nguyen <davidNg@gmail.com>
2023-08-06 00:44:42 +0700

    fix again-7

--- webassistant/urls.py
+++ webassistant/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 # a list of all the projects urls
 urlpatterns = [
@@ -8,4 +12,5 @@ urlpatterns = [
     # registering all the assistant application urls
     path('', include('assistant.urls')),
 ]
-
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
